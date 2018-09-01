0d764704d4d622ab6e031cbd7a4d9fba0edb4475
Shweta Sachdeva <shweta.sachdeva@orionesolutions.in>
2018-09-01 10:38:22 +0530

    update

--- routes/users.js
+++ routes/users.js
@@ -123,7 +123,7 @@ router.route('/register').post(function (req, res) {
 
 const configurationOptions={
   methods:['POST'],
-  origin: 'localhost:3000'
+  origin: 'https://carbazarweb2.herokuapp.com'
 }
 router.route('/getUsers').get(cors(configurationOptions),function (req, res) {
   User.find({ isdeleted: "false" }, function (err, user) {
@@ -400,139 +400,7 @@ router.route('/sendemail').get(function (req, res) {
 
 
 
-  //   router.route('/auth/forgot_password')
-  //   .get(userHandlers.render_forgot_password_template)
-  //   .post(userHandlers.forgot_password);
-
-  //   var  hbs = require('nodemailer-express-handlebars'),
-  //   email = process.env.MAILER_EMAIL_ID || 'auth_email_address@gmail.com',
-  //   pass = process.env.MAILER_PASSWORD || 'auth_email_pass'
-  //   nodemailer = require('nodemailer');
   
-  // var smtpTransport = nodemailer.createTransport({
-  //   service: process.env.MAILER_SERVICE_PROVIDER || 'Gmail',
-  //   auth: {
-  //     user: email,
-  //     pass: pass
-  //   }
-  // });
-  
-  // var handlebarsOptions = {
-  //   viewEngine: 'handlebars',
-  //   viewPath: path.resolve('./api/templates/'),
-  //   extName: '.html'
-  // };
-  
-  // smtpTransport.use('compile', hbs(handlebarsOptions));  });
-
-
-  // exports.forgot_password = function(req, res) {
-  //   async.waterfall([
-  //     function(done) {
-  //       User.findOne({
-  //         email: req.body.email
-  //       }).exec(function(err, user) {
-  //         if (user) {
-  //           done(err, user);
-  //         } else {
-  //           done('User not found.');
-  //         }
-  //       });
-  //     },
-  //     function(user, done) {
-  //       // create the random token
-  //       crypto.randomBytes(20, function(err, buffer) {
-  //         var token = buffer.toString('hex');
-  //         done(err, user, token);
-  //       });
-  //     },
-  //     function(user, token, done) {
-  //       User.findByIdAndUpdate({ _id: user._id }, { reset_password_token: token, reset_password_expires: Date.now() + 86400000 }, { upsert: true, new: true }).exec(function(err, new_user) {
-  //         done(err, token, new_user);
-  //       });
-  //     },
-  //     function(token, user, done) {
-  //       var data = {
-  //         to: user.email,
-  //         from: email,
-  //         template: 'forgot-password-email',
-  //         subject: 'Password help has arrived!',
-  //         context: {
-  //           url: 'http://localhost:3000/auth/reset_password?token=' + token,
-  //           name: user.fullName.split(' ')[0]
-  //         }
-  //       };
-  
-  //       smtpTransport.sendMail(data, function(err) {
-  //         if (!err) {
-  //           return res.json({ message: 'Kindly check your email for further instructions' });
-  //         } else {
-  //           return done(err);
-  //         }
-  //       });
-  //     }
-  //   ], function(err) {
-  //     return res.status(422).json({ message: err });
-  //   });
-  // };
-
-
-
-  // router.route('/auth/reset_password')
-  //   .get(userHandlers.render_reset_password_template)
-  //   .post(userHandlers.reset_password);
-
-  //   exports.reset_password = function(req, res, next) {
-  //     User.findOne({
-  //       reset_password_token: req.body.token,
-  //       reset_password_expires: {
-  //         $gt: Date.now()
-  //       }
-  //     }).exec(function(err, user) {
-  //       if (!err && user) {
-  //         if (req.body.newPassword === req.body.verifyPassword) {
-  //           user.hash_password = bcrypt.hashSync(req.body.newPassword, 10);
-  //           user.reset_password_token = undefined;
-  //           user.reset_password_expires = undefined;
-  //           user.save(function(err) {
-  //             if (err) {
-  //               return res.status(422).send({
-  //                 message: err
-  //               });
-  //             } else {
-  //               var data = {
-  //                 to: user.email,
-  //                 from: email,
-  //                 template: 'reset-password-email',
-  //                 subject: 'Password Reset Confirmation',
-  //                 context: {
-  //                   name: user.fullName.split(' ')[0]
-  //                 }
-  //               };
-    
-  //               smtpTransport.sendMail(data, function(err) {
-  //                 if (!err) {
-  //                   return res.json({ message: 'Password reset' });
-  //                 } else {
-  //                   return done(err);
-  //                 }
-  //               });
-  //             }
-  //           });
-  //         } else {
-  //           return res.status(422).send({
-  //             message: 'Passwords do not match'
-  //           });
-  //         }
-  //       } else {
-  //         return res.status(400).send({
-  //           message: 'Password reset token is invalid or has expired.'
-  //         });
-  //       }
-
-  //  //change password
-
-
 
 router.get('/getuserID/:_id', function (req, res) {
   User.findById( req.params._id, function (err, user) {
